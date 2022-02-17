1359e0e43977b88bfc8e931f223b11257e115ed4
BenWatts9 <brandolphwatts@gmail.com>
2022-02-17 10:50:34 -0600

    initial commit of function parameters exercise

---
+++ main.js
@@ -0,0 +1,10 @@
+const quarterValue = (num1) => {
+     const quarter = num1 / 4
+    return quarter
+}
+
+// Invoke the function and store the return value
+const newNum = quarterValue(1000)
+
+// Log the return value
+console.log(newNum)
